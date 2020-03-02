fe38ca11f89e5c66e214932d2ed08e6280aeb5bb
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2020-03-02 14:47:18 +0800

    (poll-application-django) add: polls/templates - tutorial part 3

--- poll-application-django/django/polls/views.py
+++ poll-application-django/django/polls/views.py
@@ -1,6 +1,36 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-	return HttpResponse("Hello, world")
+	# template = loader.get_template("polls/index.html")
+	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+	# output = ", ".join([q.question_text for q in latest_question_list])
+	context = {
+		"latest_question_list": latest_question_list,
+	}
+
+	# return HttpResponse(template.render(context, request))
+	return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+	# try:
+	# 	question = Question.objects.get(pk=question_id)
+	# except:
+	# 	raise Http404("Question does not exist")
+	question = get_object_or_404(Question, pk=question_id)
+
+	# return HttpResponse(f"You're looking at question {question_id}")
+	return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+	return HttpResponse(f"You're are looking at the results of question {question_id}")
+
+
+def vote(request, question_id):
+	return HttpResponse(f"You're voting on question {question_id}")

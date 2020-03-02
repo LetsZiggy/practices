ecd85902c8b530ea75ea419f4c93e983afb8c7f6
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2020-03-02 18:29:12 +0800

    [backend] add: Class Based Views - tutorial part 4

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -1,16 +1,25 @@
 from django.urls import path
 
 from .views import (
-	index,
-	detail,
-	results,
+	# index,
+	# detail,
+	# results,
 	vote,
+	IndexView,
+	DetailView,
+	ResultsView,
 )
 
 app_name = "polls"
 urlpatterns = [
-	path("", index, name="index"),
-	path("<int:question_id>/", detail, name="detail"),
-	path("<int:question_id>/results/", results, name="results"),
+	# FBV
+	# path("", index, name="index"),
+	# path("<int:question_id>/", detail, name="detail"),
+	# path("<int:question_id>/results/", results, name="results"),
 	path("<int:question_id>/vote/", vote, name="vote"),
+
+	# CBV
+	path("", IndexView.as_view(), name="index"),
+	path("<int:pk>/", DetailView.as_view(), name="detail"),
+	path("<int:pk>/results/", ResultsView.as_view(), name="results"),
 ]
